f6af693e49c709efe0f7acf21a218c765bf04235
Priyansh <priyanshpatel234@gmail.com>
2024-06-25 22:47:28 +0530

    add new file

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,43 +2,87 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import "../index.css";
 import { useAuth } from "../store/auth";
+import { useState } from "react";
 
 export const Navbar = () => {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, user } = useAuth();
+  const [element, setElement] = useState("home");
   return (
     <>
       <header>
         <div className="container navbar">
-          <div className="logo-brand">
-            <NavLink to="/">Priyansh</NavLink>
+          <div className={element === "admin" ? "active" : ""}>
+            <NavLink to="/">priyansh  </NavLink>
           </div>
 
           <nav>
             <ul>
-              <li>
-                <NavLink to="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/about">About</NavLink>
-              </li>
-              <li>
-                <NavLink to="/contact">Contact</NavLink>
-              </li>
-              <li>
-                <NavLink to="/service">Service</NavLink>
-              </li>
-              {isLoggedIn ? (
-                <li>
-                  <NavLink to="/logout">Logout</NavLink>
-                </li>
+              {user.isAdmin ? (
+                <>
+                  <li
+                    onClick={() => setElement("admin")}
+                    className={element === "admin" ? "active" : ""}
+                  >
+                    <NavLink to="/admin">Admin</NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink to="/">Home</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/about">About</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/service">Service</NavLink>
+                  </li>
+
+                  {isLoggedIn ? (
+                    <li>
+                      <NavLink to="/logout">Logout</NavLink>
+                    </li>
+                  ) : (
+                    <>
+                      <li>
+                        <NavLink to="/login">Login</NavLink>
+                      </li>
+                      <li>
+                        <NavLink to="/signup">Signup</NavLink>
+                      </li>
+                    </>
+                  )}
+                </>
               ) : (
                 <>
                   <li>
-                    <NavLink to="/login">Login</NavLink>
+                    <NavLink to="/">Home</NavLink>
                   </li>
                   <li>
-                    <NavLink to="/signup">Signup</NavLink>
+                    <NavLink to="/about">About</NavLink>
                   </li>
+                  <li>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/service">Service</NavLink>
+                  </li>
+
+                  {isLoggedIn ? (
+                    <li>
+                      <NavLink to="/logout">Logout</NavLink>
+                    </li>
+                  ) : (
+                    <>
+                      <li>
+                        <NavLink to="/login">Login</NavLink>
+                      </li>
+                      <li>
+                        <NavLink to="/signup">Signup</NavLink>
+                      </li>
+                    </>
+                  )}
                 </>
               )}
             </ul>
